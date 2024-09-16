17cc8328189c64a71257ee64eb214aa13c0928cb
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-09-16 16:17:54 +0900

    connect to bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,30 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+// import { defineBackend } from '@aws-amplify/backend';
+// import { auth } from './auth/resource';
+// import { data } from './data/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+// /**
+//  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+//  */
+// defineBackend({
+//   auth,
+//   data,
+// });
+
+import { defineBackend } from "@aws-amplify/backend";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { auth } from "./auth/resource";
+import { data, generateHaikuFunction, MODEL_ID } from "./data/resource";
+
+export const backend = defineBackend({
   auth,
   data,
+  generateHaikuFunction,
 });
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [`arn:aws:bedrock:*::foundation-model/${MODEL_ID}`],
+  })
+);
